a385790a6d0242c249f7d3f8327105f4d119c344
koop4 <dilillo.roberto@gmail.com>
2019-03-17 19:36:12 +0100

    Add delete file after creation

--- module/06_fsModule_2/app.js
+++ module/06_fsModule_2/app.js
@@ -14,13 +14,6 @@ fs.readFile(__dirname+'/simple.txt', 'utf8', (err, data) => {
     else {
         fs.writeFile(__dirname+'/writtenAsync.txt', data, (_) => {
             console.log('written!')
-
-            console.log('Now let me delete everything ok ?')
-            setTimeout( () => {
-                fs.unlink(__dirname+'/writtenAsync.txt', () => { console.log('one deleted...')})
-                fs.unlink(__dirname+'/written.txt', () => { console.log('one deleted...')});
-            }, 3000)
-            
         });
         console.log('i am probably still writing ...')
     }
